b801602b48f9766fef5def481f6e279fc503d433
mo <split@live.cn>
2015-10-27 14:15:54 +0800

    增加 solr 的插件

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var solr = require('solr-client');
+var client = solr.createClient({
+  host:'192.168.7.162',
+  port:8080,
+  path:'/solr/custom'
+});
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  /*var query = 'q=suggest:*l*&fl=hanzi&start=0&rows=10&sort='+"hanzi_weight+desc";
+  client.get('query', query, function(err, obj){
+    console.log("*********************************************************");
+    if(err){
+      console.log(err);
+    }else{
+      console.log(obj);
+    }
+  });*/
   res.render('index', { title: 'Express' });
 });
 
